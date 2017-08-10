4c7e0f7cc9a50c40eb4d8eb386c209736de23f83
jakenielson <groakayt@gmail.com>
2017-08-10 16:27:18 -0700

    Init. Added Google and NYT APIs

---
+++ js/script.js
@@ -0,0 +1,45 @@
+
+function loadData() {
+
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
+    var $street = $('#street');
+    var $city = $('#city');
+
+    // clear out old data before new request
+    $wikiElem.text("");
+    $nytElem.text("");
+    $nytElem.empty();
+
+    // load streetview
+    var url = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + $street.val() + ', ' + $city.val();
+    var $image = $('<img class="bgimg" src =' + '"' + url + '"' + '>');
+    $body.append($image);
+
+    // NYTimes
+    url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+      'api-key': "0a0ef3c641424fbe806a9fc2fd5ea166",
+      'q': $city.val()
+    });
+    $.getJSON(url, function(result){
+      console.log(result);
+      console.log(result.response.docs[0].headline.main);
+
+      for (var i = 0; i < result.response.docs.length; i++){
+        var $article = $('<li class="article"></li>');
+        $nytElem.append($article);
+        var $articleHeader = $('<a href="' + result.response.docs[i].web_url + '">' + result.response.docs[i].headline.main + '</a>');
+        var $articlePara = $('<p>' + result.response.docs[i].snippet + '</p>');
+        $('.article').last().append($articleHeader);
+        $('.article').last().append($articlePara);
+      }
+    });
+
+    return false;
+};
+
+$('#form-container').submit(loadData);
